a00ca40fc3b6e767f64712879604c7b30f4355b7
ChrisReilly024 <chris.reilly024@gmail.com>
2021-06-23 07:33:26 -0400

    contact complete

--- js/index.js
+++ js/index.js
@@ -66,7 +66,7 @@ ctaImg.src = siteContent.cta['img-src']
 //---------------------------
 
 //   Main Content   //
-   //top-content//
+//top-content//
 const header4 = document.querySelectorAll('.text-content h4')
 const mainPara = document.querySelectorAll('.main-content p')
 const midImg = document.querySelector('.middle-img')
@@ -77,7 +77,7 @@ mainPara[0].innerText = siteContent['main-content']['features-content']
 mainPara[1].innerText = siteContent['main-content']['about-content']
 document.querySelector('.middle-img').src = siteContent['main-content']['middle-img-src'];
 //---------------------------
-   //bottom-content//
+//bottom-content//
 
 header4[2].innerText = siteContent['main-content']['services-h4'];
 header4[3].innterText = siteContent['main-content']['product-h4'];
@@ -85,27 +85,12 @@ header4[4].innerText = siteContent['main-content']['vision-h4'];
 mainPara[2].innerText = siteContent['main-content'][`services-content`];
 mainPara[3].innerText = siteContent['main-content'][`product-content`];
 mainPara[4].innerText = siteContent['main-content'][`vision-content`];
+//---------------------------
 
-console.log(header4)
-// "main-content": {
-//   "features-h4":"Features",
-//   "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//   "about-h4":"About",
-//   "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//   "middle-img-src": "img/mid-page-accent.jpg",
-//   "services-h4":"Services",
-//   "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//   "product-h4":"Product",
-//   "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-//   "vision-h4":"Vision",
-//   "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-// },
-// "contact": {
-//   "contact-h4" : "Contact",
-//   "address" : "123 Way 456 Street Somewhere, USA",
-//   "phone" : "1 (888) 888-8888",
-//   "email" : "sales@greatidea.io",
-// },
-// "footer": {
-//   "copyright" : "Copyright Great Idea! 2018"
-// }
+//  Contact   //
+document.querySelector('.contact h4').innerText = siteContent['contact']['contact-h4']
+document.querySelector('.contact p').innerHTML = `${siteContent['contact']['address']},<br>${siteContent['contact']['phone']},<br>${siteContent['contact']['email']}`
+
+  // "footer": {
+    //   "copyright" : "Copyright Great Idea! 2018"
+    // }
