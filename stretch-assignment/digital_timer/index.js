1d0996cc92200f1cceaeca8413126d8e6be0c7d4
ChrisReilly024 <chris.reilly024@gmail.com>
2021-06-24 01:57:24 -0400

    timer stop condition added, output styled red when timer stops on 10s

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -1,18 +1,42 @@
 let tens = document.getElementById('secondTens');
 let seconds = document.getElementById('secondOnes');
-let msTens = document.getElementById('msTens');
-let msHundreds = document.getElementById('msHundreds')
+let tenths = document.getElementById('tenths');
+let hundreths = document.getElementById('hundreths')
+let digits = document.querySelectorAll('.digit')
 
 tens.innerText = 0
 seconds.innerText = 0
-msTens.innerText = 0
-msHundreds.innerText = 0
+tenths.innerText = 0
+hundreths.innerText = 0
 
 
 function timer() {
-    if (+msHundreds.innerText === 9) msTens.innerText = +msTens.innerText + 1;
-    msHundreds.innerText = +msHundreds.innerText !== 9 ? +msHundreds.innerText + 1 : 0
+    if(+tens.innerText === 1) {
+        hundreths.innerText = 0    
+        tenths.innerText = 0    
+        seconds.innerText = 0    
+        digits.forEach(digit => digit.style.color = 'red');
+        return tenths;
+    };
+    
+
+    if (+seconds.innerText === 9 && +tenths.innerText === 9) {
+        tens.innerText = +tens.innerText + 1
+    }
+
+    if (+tenths.innerText === 9 && +hundreths.innerText === 9) {
+        seconds.innerText = +seconds.innerText + 1
+    }
+
+    if (+hundreths.innerText === 9) {
+        tenths.innerText = +tenths.innerText === 9 ? 0 : +tenths.innerText + 1
+    }
+
+    
+    hundreths.innerText = +hundreths.innerText === 9 ? 0 : +hundreths.innerText + 1;
+
+
 }
-setInterval(timer, 1000)
 
 
+setInterval(timer, 10)
