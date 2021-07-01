716f416659069fc6f0677f028ab8b60aaff323ad
ChrisReilly024 <chris.reilly024@gmail.com>
2021-07-01 10:40:47 -0400

    start btn disabled on click, re-enabled on stop clicked

--- stretch-assignment/digital_timer/index.js
+++ stretch-assignment/digital_timer/index.js
@@ -3,15 +3,17 @@ const seconds = document.getElementById('secondOnes');
 const tenth = document.getElementById('tenth');
 const hundreths = document.getElementById('hundred');
 const digits = document.querySelectorAll('.digit');
+const colon = document.querySelector('.colon')
 
-const resetBtn = document.querySelector('.reset')
-const lapBtn = document.querySelector('.lapBtn')
-const logTimes = document.querySelector('.times')
-const startBtn = document.querySelector('.startBtn')
-const stopBtn = document.querySelector('.stopBtn')
+const resetBtn = document.querySelector('.reset');
+const lapBtn = document.querySelector('.lapBtn');
+const logTimes = document.querySelector('.times');
+const startBtn = document.querySelector('.startBtn');
+const stopBtn = document.querySelector('.stopBtn');
 
-var inter
+var inter;
 var i = 0;
+var running = false;
 
 tens.innerText = 0;
 seconds.innerText = 0;
@@ -25,8 +27,7 @@ const reseter = () => {
     hundreths.innerText = 0;
     logTimes.innerHTML = '';
     i = 0;
-}
-
+};
 const timer = () => {
     if (+seconds.innerText === 9 && +tenth.innerText === 9 && +hundreths.innerText === 9) {
         tens.innerText = +tens.innerText === 9 ? 0 : +tens.innerText + 1;
@@ -38,7 +39,7 @@ const timer = () => {
         tenth.innerText = +tenth.innerText === 9 ? 0 : +tenth.innerText + 1;
     }
     hundreths.innerText = +hundreths.innerText === 9 ? 0 : +hundreths.innerText + 1;
-}
+};
 const lapTimes = () => {
     i += 1
     let time = `${tens.innerText}${seconds.innerText}:${tenth.innerText}${hundreths.innerText}`
@@ -46,54 +47,19 @@ const lapTimes = () => {
     lapTime.classList.add(`lap${i}`, `lap`);
     logTimes.appendChild(lapTime);
     lapTime.innerText = `Lap ${i}: ${time}`;
+};
+const start = () => {
+    startBtn.disabled = true;
+    inter = setInterval(timer, 10);
 }
-
-const start = () => inter = setInterval(timer, 10);
-const stop = () => clearInterval(inter)
+const stop = () => {
+    startBtn.disabled = false;
+    clearInterval(inter)
+};
 
 startBtn.addEventListener('click', start);
 stopBtn.addEventListener('click', stop);
 resetBtn.addEventListener('click', reseter);
-lapBtn.addEventListener('click', lapTimes)
-//-------------------------------------------
-
-
-
-///--------------------------------------------
-// function timer() {
-//     if(+tens.innerText === 1) {
-//         hundreths.innerText = 0;    
-//         tenths.innerText = 0;    
-//         seconds.innerText = 0;    
-//         digits.forEach(digit => digit.style.color = 'red');
-//         return tenths;
-//     };
-      
-//     if(+seconds.innerText === 1) digits.forEach(digit => digit.style.color = 'rgb(5, 70, 5)');
-//     if(+seconds.innerText === 2) digits.forEach(digit => digit.style.color = 'rgb(15, 121, 15)');
-//     if(+seconds.innerText === 3) digits.forEach(digit => digit.style.color = 'rgb(39, 138, 39)');
-//     if(+seconds.innerText === 4) digits.forEach(digit => digit.style.color = 'rgb(69,165,69)');
-//     if(+seconds.innerText === 5) digits.forEach(digit => digit.style.color = 'rgb(152,211,152)');
-//     if(+seconds.innerText === 6) digits.forEach(digit => digit.style.color = 'rgb(184,197,124)');
-//     if(+seconds.innerText === 7) digits.forEach(digit => digit.style.color = 'rgb(184, 146, 41)');
-//     if(+seconds.innerText === 8) digits.forEach(digit => digit.style.color = 'rgb(218, 128, 26)');
-//     if(+seconds.innerText === 9) digits.forEach(digit => digit.style.color = 'rgb(243, 94, 8)');
-    
-//     if (+seconds.innerText === 9 && +tenths.innerText === 9) {
-//         tens.innerText = +tens.innerText + 1;
-//     };
-//
-//     if (+tenths.innerText === 9 && +hundreths.innerText === 9) {
-//         seconds.innerText = +seconds.innerText + 1;
-//     };
-//
-//     if (+hundreths.innerText === 9) {
-//         tenths.innerText = +tenths.innerText === 9 ? 0 : +tenths.innerText + 1;
-//     };
-//    
-//     hundreths.innerText = +hundreths.innerText === 9 ? 0 : +hundreths.innerText + 1;
-// }
-
-// setInterval(timer, 10)
-
-
+lapBtn.addEventListener('click', lapTimes);
+console.log(startBtn.attributes)
+//-------------------------------------------//
